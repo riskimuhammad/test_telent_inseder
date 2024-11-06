5e6a3231fcbb9be7cfa932b81eceb9482b77dc96
riskimuhammad <riskimuhammad8921@gmail.com>
2024-11-06 20:31:36 +0700

    setup binding

--- lib/domain/bindings/signin_bindings.dart
+++ lib/domain/bindings/signin_bindings.dart
@@ -1,9 +1,21 @@
 import 'package:get/get.dart';
 import 'package:test_muhammad_riski/presentation/controller/signin/signin_controller.dart';
+import '../../data/provider/network/contract.dart';
+import '../../data/provider/network/http/http_contract.dart';
+import '../../data/provider/network/http/http_implementation.dart';
+import '../../data/provider/network/implementation.dart';
+import '../../data/repository_impl/repository_impl.dart';
+import '../repository/repository.dart';
 
 class SigninBindings implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<SigninController>(() => SigninController(), fenix: true);
+    Get.lazyPut<HttpNetwork>(() => HttpNetworkImplementation(), fenix: true);
+    Get.lazyPut<Network>(() => NetworkImplementation(Get.find<HttpNetwork>()));
+    Get.lazyPut<Repository>(
+        () => RepositoryImplementation(Get.find<Network>()));
+    Get.lazyPut<SigninController>(
+        () => SigninController(Get.find<Repository>()),
+        fenix: true);
   }
 }
--- lib/presentation/controller/signin/signin_controller.dart
+++ lib/presentation/controller/signin/signin_controller.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:test_muhammad_riski/domain/repository/repository.dart';
 
 class SigninController extends GetxController {
+  Repository repository;
+  SigninController(this.repository);
+
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 }
